7aa81bec3b7be3b62e2405c0b6aa66a32421edfe
Utkarsh <utkarshrenukdas2003@gmail.com>
2025-03-24 12:11:29 +0530

    database query

--- init/index.js
+++ init/index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = process.env.MONGO_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
   .then(() => {
